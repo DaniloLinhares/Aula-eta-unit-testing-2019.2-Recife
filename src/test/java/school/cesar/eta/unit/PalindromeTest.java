94ac4bdb24f5c46f64ffc33b94b610a2774a34ca
Danilo <daniloferreira2004@gmail.com>
2020-07-25 12:23:05 -0300

    refatorando Palindrome Test

--- src/test/java/school/cesar/eta/unit/PalindromeTest.java
+++ src/test/java/school/cesar/eta/unit/PalindromeTest.java
@@ -1,22 +1,30 @@
 package school.cesar.eta.unit;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PalindromeTest {
+
+    private Palindrome palindrome;
+    @BeforeEach
+        public void setupTest(){
+            palindrome = new Palindrome();
+    }
+
     @Test
     public void palindrome_singleLetter_true(){
-        boolean result = Palindrome.check("a");
+        boolean result = palindrome.check("a");
         Assertions.assertTrue(result);
     }
     @Test
-    public void palindrome_notPalindromeWord_false(){
-        boolean result = Palindrome.check("ab");
+    public void palindrome_PalindromeWord_false(){
+        boolean result = palindrome.check("ab");
         Assertions.assertFalse(result);
     }
     @Test
-    public void palindrome_notPalindromeWord_true(){
-        boolean result = Palindrome.check("renner");
+    public void palindrome_PalindromeWord_true(){
+        boolean result = palindrome.check("renner");
         Assertions.assertTrue(result);
     }
 }
